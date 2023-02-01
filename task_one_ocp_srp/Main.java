c63ea51952e2488c03ab66453a2255d8e64a101c
supreme1444 <107640724+supreme1444@users.noreply.github.com>
2023-02-01 08:32:47 +0300

    Create Main.java

---
+++ task_one_ocp_srp/Main.java
@@ -0,0 +1,11 @@
+package job.task_one_ocp_srp;
+
+public class Main {
+    public static void main(String[] args) {
+        SuperPhone superPhone = new SuperPhone();
+        superPhone.mode();
+        superPhone.pin();
+        superPhone.fingerprint();
+        superPhone.faceID();
+    }
+}
