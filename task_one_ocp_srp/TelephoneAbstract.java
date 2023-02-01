59648783f1577b6bfbaf014cbfc7437059d25ea5
supreme1444 <107640724+supreme1444@users.noreply.github.com>
2023-02-01 08:33:57 +0300

    Create TelephoneAbstract.java

---
+++ task_one_ocp_srp/TelephoneAbstract.java
@@ -0,0 +1,17 @@
+package job.task_one_ocp_srp;
+
+public abstract class TelephoneAbstract implements InterfacePin, InterfaceFingerprint, IntrfaceFaceID {
+
+    @Override
+    public void mode(){};
+
+    @Override
+    public void pin(){};
+
+    @Override
+    public void fingerprint(){};
+
+    @Override
+    public void faceID(){};
+    
+}
