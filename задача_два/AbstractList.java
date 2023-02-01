e6b5476e5c83360235cc9800052be71c47643846
supreme1444 <107640724+supreme1444@users.noreply.github.com>
2023-02-01 08:34:56 +0300

    Create AbstractList.java

---
+++ задача_два/AbstractList.java
@@ -0,0 +1,13 @@
+package job.task_two;
+
+import java.util.List;
+
+public abstract class AbstractList<T> {
+
+    public List<T> spisok;
+
+    public void getSize(){};
+    public void getInt(int i){};
+    public void spisok(){};
+
+}
